cf72242b2e036e065abe0270a7c081b5a90ba268
shivam30CJ <162012746+shivam30CJ@users.noreply.github.com>
2024-04-06 15:53:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let x= document.getElementById("popup");
+function openx(){
+    x.classList.add("open-x");
+}
+function closex(){
+    x.classList.remove("open-x");
+}
+
+
+function myFunction1() {
+    var x = document.getElementById("pop1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("pop2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("last");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
